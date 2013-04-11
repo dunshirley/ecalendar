4695dffce1688af06e10456b0726fa1fa1ee1305
sunliang <i@liangsun.org>
2013-04-11 15:35:25 +0800

    Modify models.

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from app.models import *
 
-admin.site.register(Activity)
-admin.site.register(Tag)
 admin.site.register(Calendar)
+admin.site.register(City)
+admin.site.register(Tag)
+admin.site.register(Device)
+admin.site.register(Activity)
+admin.site.register(Reaction)
+admin.site.register(Feedback)
+
 
--- app/models.py
+++ app/models.py
@@ -1,6 +1,34 @@
 from django.db import models
 
+class Calendar(models.Model):
+    date = models.DateField()
+    name = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return str(self.date) + '(' + self.name + ')'
+
+
+class City(models.Model):
+    pinyin = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.name
+
+class Device(models.Model):
+    identification = models.CharField(max_length=100)
+    KIND_OS = (
+            ('a', 'Aandroid'),
+            ('i', 'iOS'),
+            )
+    os = models.CharField(max_length=1, choices=KIND_OS)
+
 class Activity(models.Model):
+    city = models.ForeignKey(City)
+
     title = models.CharField(max_length=2000)
     content = models.CharField(max_length=10000)
     url = models.CharField(max_length=1000)
@@ -18,18 +46,11 @@ class Activity(models.Model):
 
     def __unicode__(self):
         return self.title
-    
 
-class Tag(models.Model):
-    name = models.CharField(max_length=20)
+class Reaction(models.Model):
+    activity = models.ForeignKey(Activity)
+    device = models.ForeignKey(Device)
 
-    def __unicode__(self):
-        return self.name
-
-class Calendar(models.Model):
-    date = models.DateField()
-    name = models.CharField(max_length=20)
-
-    def __unicode__(self):
-        return str(self.date) + '(' + self.name + ')'
-
+class Feedback(models.Model):
+    content = models.CharField(max_length=10000)
+    device = models.ForeignKey(Device)
