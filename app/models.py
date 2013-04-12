8faf160cdb87b1c22778b6bf343e1e7099004384
sunliang <i@liangsun.org>
2013-04-12 10:37:05 +0800

    Fix models.

--- app/models.py
+++ app/models.py
@@ -12,6 +12,9 @@ class City(models.Model):
     pinyin = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return self.name
+
 class Tag(models.Model):
     name = models.CharField(max_length=20)
 
@@ -26,18 +29,21 @@ class Device(models.Model):
             )
     os = models.CharField(max_length=1, choices=KIND_OS)
 
+    def __unicode__(self):
+        return self.os + '_' + self.identification
+
 class Activity(models.Model):
     city = models.ForeignKey(City)
 
     title = models.CharField(max_length=2000)
-    content = models.CharField(max_length=10000)
+    content = models.TextField()
     url = models.CharField(max_length=1000)
     source = models.CharField(max_length=100)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
     weight = models.IntegerField(blank=True, null=True)
-    public = models.NullBooleanField()
+    public = models.BooleanField(default=True)
 
     created_time = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now_add=True)
@@ -51,6 +57,16 @@ class Reaction(models.Model):
     activity = models.ForeignKey(Activity)
     device = models.ForeignKey(Device)
 
+    like = models.NullBooleanField()
+    dislike = models.NullBooleanField()
+    clicked = models.NullBooleanField()
+
+    def __unicode__(self):
+        return self.activity
+
 class Feedback(models.Model):
     content = models.CharField(max_length=10000)
     device = models.ForeignKey(Device)
+
+    def __unicode__(self):
+        return self.content[:20]
