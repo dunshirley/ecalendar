11c5db980e9ebdd6c38c05c17b90c78a3dbad443
sunliang <i@liangsun.org>
2013-04-14 21:44:44 +0800

    Remove useless file.

--- django-admin.py
+++
@@ -1,6 +0,0 @@
-from django.core import management
-import os
-import sys
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csvimport.tests.settings")
-if __name__ == "__main__":
-    management.execute_from_command_line(sys.argv)
