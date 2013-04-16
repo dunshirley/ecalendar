d0f3b553ed100ff8aef4ccf928988b7aa66ca971
sunliang <i@liangsun.org>
2013-04-17 00:38:47 +0800

    Add reaction interface.

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,9 @@ from app.views import *
 from app.models import *                                                       
 
 urlpatterns = patterns('app.views', 
-    url(r'^calendar/$', 'calendar'),
+    url(r'^calendar/?$', 'calendar'),
     url(r'^activities/?$', 'activities'),
+    url(r'^reaction/?$', 'reaction'),
+    url(r'^feedback/?$', 'feedback'),
+    url(r'^app/?$', 'download'),
 )
