53eacda9ed4351f481fb26048012d70c81e17faf
Liang Sun <i@liangsun.org>
2013-05-18 10:50:18 +0800

    Add new event url.

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url                                                                                                                               
+from django.views.generic import CreateView
 
 from app.views import *                                                        
 from app.models import *                                                       
+
 
 urlpatterns = patterns('app.views', 
     url(r'^calendar/?$', 'calendar'),
@@ -11,4 +13,6 @@ urlpatterns = patterns('app.views',
     url(r'^app/?$', 'update'),
     url(r'^bot/?$', 'bot'),
     url(r'^huodongrili.apk$', 'download'),
+
+    url(r'^new_event/?$', CreateView.as_view(model=Activity)),
 )
